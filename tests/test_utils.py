dad6be1605653a18e19b67a5c201f323c8034f1f
Ryan Hunter <ryanhunter237@gmail.com>
2021-11-19 14:59:35 -0500

    Add 9-stone handicap test for fixing handicap sgf strings

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,3 +19,10 @@ class TestFixingHandicapGameStrings(unittest.TestCase):
         correct_game_str = self.get_sgf_str('handicap2_corrected.sgf')
         fixed_game_str = utils.fix_handicap_game_str(game_str)
         self.assertEqual(correct_game_str, fixed_game_str)
+
+    def test_correct_NINE_handicap(self):
+        self.maxDiff = None
+        game_str = self.get_sgf_str('handicap9_error.sgf')
+        correct_game_str = self.get_sgf_str('handicap9_corrected.sgf')
+        fixed_game_str = utils.fix_handicap_game_str(game_str)
+        self.assertEqual(correct_game_str, fixed_game_str)
