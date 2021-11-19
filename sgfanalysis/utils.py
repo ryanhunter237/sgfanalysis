08c34cfb7d34a736733a20b687e0f50b380c91f8
Ryan Hunter <ryanhunter237@gmail.com>
2021-11-19 15:06:54 -0500

    Add functions for loading game string and extracting metadata

--- sgfanalysis/utils.py
+++ sgfanalysis/utils.py
@@ -1,7 +1,11 @@
 import itertools, re
 from sgfmill import sgf
 
-def fix_handicap_game_str(game_str):
+property2key = {'PC':'url', 'DT':'date', 'PB':'bname', 'PW':'wname', 'BR':'brank', 'WR':'wrank',
+                'TM':'main_time', 'OT':'overtime', 'RE':'result', 'SZ':'size', 'HA':'handicap', 
+                'KM':'komi', 'RU':'ruleset', 'GC':'game_type'}
+
+def fix_handicap_game_string(game_str):
     i = re.search(r'AB\[[a-z]{2}\]', game_str).start()
     game_str_start = game_str[:i]
     game_str_end = game_str[i:]
@@ -10,3 +14,29 @@ def fix_handicap_game_str(game_str):
     placement_strings = 'AB' + ''.join(moves[:int(handicap)]) 
     move_strings = [''.join(x) for x in zip(itertools.cycle([';W',';B']), moves[int(handicap):])]
     return game_str_start + placement_strings + ''.join(move_strings) + ')'
+
+def get_game_string(path):
+    with open(path, encoding="utf-8") as f:
+        game_str = f.read()
+    if re.search(r"\[ZZ\]", game_str):
+        raise RuntimeError("Sgf file contains invalid [ZZ] move")
+    if re.search(r"HA\[[0-9]{1,2}\]", game_str):     
+        if not re.search(r"AB\[[a-z]{2}\]", game_str):
+            raise RuntimeError("Sgf file has handicap, but no handicap stones")
+        game_str = fix_handicap_game_string(game_str)
+    return game_str
+
+def get_metadata(game):
+    metadata = {}
+    root = game.get_root()
+    for key, value in property2key.items():
+        try:
+            metadata[value] = root.get(key)
+        except (KeyError, ValueError):
+            metadata[value] = None
+    metadata['url'] = metadata['url'][5:]
+    metadata['num_moves'] = len(game.get_main_sequence()) - 1
+    game_type, ranked = metadata['game_type'].split(",")
+    metadata['game_type'] = game_type
+    metadata['ranked'] = (ranked == "ranked")
+    return metadata
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,19 +10,19 @@ class TestFixingHandicapGameStrings(unittest.TestCase):
 
     def test_no_change_one_handicap(self):
         game_str = self.get_sgf_str('handicap1.sgf')
-        fixed_game_str = utils.fix_handicap_game_str(game_str)
+        fixed_game_str = utils.fix_handicap_game_string(game_str)
         self.assertEqual(game_str, fixed_game_str)
 
     def test_correct_two_handicap(self):
         self.maxDiff = None
         game_str = self.get_sgf_str('handicap2_error.sgf')
         correct_game_str = self.get_sgf_str('handicap2_corrected.sgf')
-        fixed_game_str = utils.fix_handicap_game_str(game_str)
+        fixed_game_str = utils.fix_handicap_game_string(game_str)
         self.assertEqual(correct_game_str, fixed_game_str)
 
     def test_correct_NINE_handicap(self):
         self.maxDiff = None
         game_str = self.get_sgf_str('handicap9_error.sgf')
         correct_game_str = self.get_sgf_str('handicap9_corrected.sgf')
-        fixed_game_str = utils.fix_handicap_game_str(game_str)
+        fixed_game_str = utils.fix_handicap_game_string(game_str)
         self.assertEqual(correct_game_str, fixed_game_str)
