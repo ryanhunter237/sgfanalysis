921cfaac3fada51a87ef053368c63164f145231e
Ryan Hunter <ryanhunter237@gmail.com>
2021-11-19 14:47:51 -0500

    Keep passes when fixing handicap sgf strings

--- sgfanalysis/utils.py
+++ sgfanalysis/utils.py
@@ -6,7 +6,7 @@ def fix_handicap_game_str(game_str):
     game_str_start = game_str[:i]
     game_str_end = game_str[i:]
     handicap = int(re.search(r'HA\[([0-9]+)\]', game_str_start).group(1))
-    moves = re.findall(r"\[[a-z]{2}\]", game_str_end)
+    moves = re.findall(r"(?:\[[a-z]{2}\]|\[\])", game_str_end)
     placement_strings = 'AB' + ''.join(moves[:int(handicap)]) 
     move_strings = [''.join(x) for x in zip(itertools.cycle([';W',';B']), moves[int(handicap):])]
     return game_str_start + placement_strings + ''.join(move_strings) + ')'
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,19 @@ import os, unittest
 from sgfanalysis import utils
 
 class TestFixingHandicapGameStrings(unittest.TestCase):
-    def test_correct_string(self):
-        with open(os.path.join('tests', 'files', 'handicap1.sgf'), encoding='utf-8') as f:
+    def get_sgf_str(self, filename):
+        with open(os.path.join('tests', 'files', filename), encoding='utf-8') as f:
             game_str = f.read()
+        return game_str
+
+    def test_no_change_one_handicap(self):
+        game_str = self.get_sgf_str('handicap1.sgf')
         fixed_game_str = utils.fix_handicap_game_str(game_str)
         self.assertEqual(game_str, fixed_game_str)
+
+    def test_correct_two_handicap(self):
+        self.maxDiff = None
+        game_str = self.get_sgf_str('handicap2_error.sgf')
+        correct_game_str = self.get_sgf_str('handicap2_corrected.sgf')
+        fixed_game_str = utils.fix_handicap_game_str(game_str)
+        self.assertEqual(correct_game_str, fixed_game_str)
